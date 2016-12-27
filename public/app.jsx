f7a7eb0a3afd3e1378422ae04f6cdc15ce916d44
Patricia Arbona <patricia.arbona@omadahealth.com>
2016-12-26 23:12:56 -0500

    Continuing React tutorial

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,53 @@
+  var Greeter = React.createClass({
+    getDefaultProps: function () {
+      return {
+        name: 'Denizen',
+        message: 'I love 🍒 Princess BubbleGum!'
+      };
+    },
+    getInitialState: function() {
+      return {
+        name: this.props.name
+      };
+    },
+
+    onButtonClick: function (e) {
+      e.preventDefault();
+
+      var nameRef = this.refs.name;
+
+
+      var name = nameRef.value;
+      nameRef.value = '';
+
+      if (typeof name === 'string' && name.length > 0) {
+        this.setState({
+          name: name
+        });
+      }
+    },
+
+    render: function () {
+      var name = this.state.name;
+      var message = this.props.message;
+      return (
+        <div>
+          <h1>Hello {name}!</h1>
+          <p> {message + '!!!!💙'} </p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+        <button>Set Name</button>
+        </form>
+        </div>
+      );
+    }
+  });
+
+  var firstName = 'Patricia';
+  var anotherMessage = "I love React"
+
   ReactDOM.render(
-    <h1>Hello React!</h1>,
+    <Greeter name={firstName}/>,
     document.getElementById('app')
   );
