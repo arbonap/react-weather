517b298d748e58b49a5d7af4b3639c9e731e3fa5
Patricia Arbona <patricia.arbona@omadahealth.com>
2016-12-27 21:12:31 -0500

    Separate components into different files for scalability

--- public/app.jsx
+++ public/app.jsx
@@ -1,85 +1,9 @@
-var GreeterMessage = React.createClass({
-  render: function () {
-    var name = this.props.name;
-    var message = this.props.message;
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('./components/Greeter');
 
-    return (
-      <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}!</p>
-      </div>
-    );
-  }
-});
+var firstName = 'Patricia';
+var anotherMessage = "I love React"
 
-var GreeterForm = React.createClass({
-  onFormSubmit: function(e) {
-    e.preventDefault();
-
-    var updates = {};
-    var name = this.refs.name.value;
-    var message = this.refs.message.value;
-
-    if (name.length > 0) {
-      this.refs.name.value = '';
-      updates.name = name;
-
-    }
-
-    if (message.length > 0) {
-      this.refs.message.value = '';
-      updates.message = message;
-    }
-
-    this.props.onNewData(updates);
-  },
-  render: function () {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <div><input type="text" ref="name" placeholder="Enter name"/></div>
-      <div><textarea ref="message" placeholder="Enter message"></textarea></div>
-    <div><button>Submit</button></div>
-      </form>
-    );
-  }
-})
-
-  var Greeter = React.createClass({
-    getDefaultProps: function () {
-      return {
-        name: 'Denizen',
-        message: 'I love 🍒 Princess BubbleGum!'
-      };
-    },
-    getInitialState: function() {
-      return {
-        name: this.props.name,
-        message: this.props.message
-      };
-    },
-
-    handleNewData: function (updates) {
-      this.setState(updates);
-    },
-
-    render: function () {
-      var name = this.state.name;
-      var message = this.state.message;
-
-      return (
-        <div>
-
-      <GreeterMessage name={name} message={message}/>
-      <GreeterForm onNewData={this.handleNewData}/>
-        </div>
-      );
-    }
-  });
-
-  var firstName = 'Patricia';
-  var anotherMessage = "I love React"
-
-  ReactDOM.render(
-    <Greeter name={firstName}/>,
-    document.getElementById('app')
-  );
+ReactDOM.render(
+    <Greeter name={firstName}/>, document.getElementById('app'));
